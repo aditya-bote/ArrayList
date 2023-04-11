88372b78c05bbc55f30d70eeb4ea55632dd896ae
Aditya Pramod Bote <adityabot@cybage.com>
2023-04-11 15:19:57 +0530

    First commit

---
+++ Arraylist/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace arraylist
+{
+    /*internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            al.Add(1);
+            al.Add(2);
+            al.Add(3);
+            al.Add(5);
+            al.Add(5);
+            foreach(var i in al)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("capacity of al : " + al.Capacity);
+            Console.WriteLine("number of elements in al : " + al.Count);
+            Console.WriteLine("index of al:" + al.IndexOf(2));
+
+            al.Remove(2);
+            foreach(var i in al)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("remaining elements in al now ..." + al);
+            al.Insert(1, 6);
+            foreach(var i in al)
+            {
+                Console.WriteLine(i);
+            }
+            
+            Console.ReadLine();
+        }
+    }*/
+
+    /*class program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add(1, "one");
+            ht.Add(2, "two");
+            ht.Add(3, "three");
+            ht.Add(4, "four");
+            ht.Add(5, "five");
+            foreach(DictionaryEntry i in ht)
+            {
+                Console.WriteLine(i.Key + "" + i.Value);
+            }
+            ht.Remove(1);
+
+            foreach(DictionaryEntry i in ht)
+            {
+                Console.WriteLine(i.Key + "" + i.Value);
+            }
+
+            ht.Contains("one");
+            Console.WriteLine(ht.ContainsKey(3));
+            Console.WriteLine(ht.ContainsValue("five"));
+            Console.WriteLine("total items in " + ht.Count);
+            ht.Add(3, "nine");
+            Console.WriteLine("total elements are .. ");
+            Console.WriteLine(ht);
+
+            Console.ReadLine();
+
+        }
+    }*/
+
+    /*class program
+    {
+        static void Main(string[] args)
+        {
+            List<int> MyList = new List<int>();
+            MyList.Add(1);
+            MyList.Add(2);
+            MyList.Add(3);
+            MyList.Add(4);
+            MyList.Add(5);
+
+            foreach(var i in MyList)
+            {
+                Console.WriteLine(i);
+            }
+
+            MyList.Sort();
+            Console.WriteLine("elements of the list are : " + MyList);
+            int c = MyList.Max();
+            Console.WriteLine("maximum number in the list " + c);
+            Console.ReadLine();
+
+        }
+    }*/
+    
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
